2997a28d8b61708b14982cf09cecbc69fe71535c
Ekaterina <santeh.lg@gmail.com>
2023-07-30 13:38:22 +0300

    Скрипты к ДЗ по JavaScript

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function my_name(){
+  let name = ("Екатерина");
+  while (true){
+    let nam = prompt("Введите имя");
+    if (name === nam) break;
+  }
+  alert('Здравствуйте, Екатерина!');
+  document.write(" Хорошей работы !!!");
+}
+my_name()
